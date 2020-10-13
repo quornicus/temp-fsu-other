73faa0311f11a445cb883a062f8fcfdae81c92b9
Malcolm Shackelford <mshackelford@fsu.edu>
2020-10-13 12:29:13 -0400

    Fixed.

--- isle-components.sh
+++ isle-components.sh
@@ -14,29 +14,29 @@ function apache_up {
         --name apache \
         --mount type=bind,src=/home/ubuntu/data/apache/html,dst=/var/www/html \
         --mount type=bind,src=/home/ubuntu/data/apache/settings.php,dst=/var/www/html/sites/default/settings.php \
-        --env-file /home/ubuntu/envs.env \
+        --env-file /home/ubuntu/temp-fsu-other/all-envs.env \
         ${SET_BG} fsu-apache:1.0
 }
 
 function fedora_up {
     [[ ${1} ]] && local SET_BG="-d"
-    docker run -p 8081:8080 \
+    docker run -p 8080:8080 \
         --name fedora \
         --mount type=bind,src=/home/ubuntu/data/fedora/datastreamStore,dst=/usr/local/fedora/data/datastreamStore \
         --mount type=bind,src=/home/ubuntu/data/fedora/objectStore,dst=/usr/local/fedora/data/objectStore \
         --mount type=bind,src=/home/ubuntu/data/fedora/akubra-llstore.xml,dst=/usr/local/fedora/server/config/spring/akubra-llstore.xml \
         --mount type=bind,src=/home/ubuntu/data/fedora/gsearch/RELS-EXT_to_solr.xslt,dst=/usr/local/tomcat/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/islandora_transforms/RELS-EXT_to_solr.xslt \
         --mount type=bind,src=/home/ubuntu/data/fedora/gsearch/foxmlToSolr.xslt,dst=/usr/local/tomcat/webapps/fedoragsearch/WEB-INF/classes/fgsconfigFinal/index/FgsIndex/foxmlToSolr.xslt \
-        --env-file /home/ubuntu/fedora.env \
+        --env-file /home/ubuntu/temp-fsu-other/all-envs.env \
         ${SET_BG} fsu-fedora:1.0
 }
 
 function blaze_up {
     [[ ${1} ]] && local SET_BG="-d"
-    docker run -p 8084:8080 \
+    docker run -p 8080:8080 \
         --name blaze \
         --mount type=bind,src=/home/ubuntu/data/bigdata,dst=/var/bigdata \
-        --env-file /home/ubuntu/envs.env \
+        --env-file /home/ubuntu/temp-fsu-other/all-envs.env \
         ${SET_BG} islandoracollabgroup/isle-blazegraph:1.5.1
 }
 
@@ -52,10 +52,10 @@ function mysql_up {
 
 function solr_up {
     [[ ${1} ]] && local SET_BG="-d"
-    docker run -p 8082:8080 \
+    docker run -p 8080:8080 \
         --name solr \
         --mount type=bind,src=/home/ubuntu/data/solr/data,dst=/usr/local/solr/collection1/data \
-        --env-file /home/ubuntu/envs.env \
+        --env-file /home/ubuntu/temp-fsu-other/all-envs.env \
         ${SET_BG} islandoracollabgroup/isle-solr:1.5.1
 }
 
